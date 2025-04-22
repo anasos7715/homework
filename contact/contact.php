40eaaf409033d4f135fbddf5cbdc8714757e0c0b
anasos7715 <anasshabi56@gmail.com>
2025-04-22 21:32:53 +0300

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Thank You</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -24,51 +25,57 @@
 if (!isset($_POST['submit'])) {
     die('Please submit the form.');
 }
+$name = $_POST['myname'] ;
+$email = $_POST['email'] ;
+$gender = $_POST['gender'] ;
+$inquiry_type = $_POST['inquiry_type'] ;
 
-$name = $_POST['myname'];
-$email = $_POST['email'];
-$gender = $_POST['gender'];
-$inquiry_type = $_POST['inquiry_type'];
-
-
-// الاتصال بقاعدة البيانات
-$con = mysqli_connect('localhost', 'root', '', 'dummy');
-if (!$con) {
-    die('<h3>Could not connect to the database!</h3>');
+if (!preg_match('/^[[:alnum:]]+@[[:alnum:]]+\.com$/i', $email)) {
+    die('<div class="alert alert-danger">Email is invalid. Please use an email ending in .com with alphanumeric characters.</div>');
 }
 
-// إدخال البيانات باستخدام Prepared Statement
+// Connect to database
+$con = mysqli_connect('localhost', 'root', '', 'dummy');
+if (!$con) {
+    die('<div class="alert alert-danger">Could not connect to the database!</div>');
+}
+
+// Prepare and execute query
 $stmt = $con->prepare("INSERT INTO contacts (name, email, gender, inquiry_type) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("ssss", $name, $email, $gender, $inquiry_type);
 
 if (!$stmt->execute()) {
     $stmt->close();
     mysqli_close($con);
-    die('<h3>Could not execute query!</h3>');
+    die('<div class="alert alert-danger">Could not execute query!</div>');
 }
 
-// استرجاع آخر ID
+// Get last inserted ID
 $last_id = mysqli_insert_id($con);
 
 $stmt->close();
 mysqli_close($con);
 ?>
 
-<h1>Thank you</h1>
-<p class="visitor">You are visitor number: <?php echo htmlspecialchars($last_id); ?></p>
-<p>You provided us with the following info:</p>
-<p>Name: <?php echo htmlspecialchars($name); ?></p>
-<p>Email: <?php echo htmlspecialchars($email); ?></p>
-<p>Gender: <?php echo ($gender == 1) ? 'Male' : 'Female'; ?></p>
-<p>Inquiry Type: 
-    <?php 
-    $inquiry_types = [
-        'support' => 'Support',
-        'sales' => 'Sales',
-        'feedback' => 'Feedback'
-    ];
-    echo htmlspecialchars($inquiry_types[$inquiry_type]);
-    ?>
-</p>
+<div class="p-3">
+    <h1 class="text-success">Thank you</h1>
+    <p class="visitor">You are visitor number: <?php echo htmlspecialchars($last_id); ?></p>
+    <p>You provided us with the following info:</p>
+    <ul class="list-group">
+        <li class="list-group-item">Name: <?php echo htmlspecialchars($name); ?></li>
+        <li class="list-group-item">Email: <?php echo htmlspecialchars($email); ?></li>
+        <li class="list-group-item">Gender: <?php echo ($gender == 1) ? 'Male' : 'Female'; ?></li>
+        <li class="list-group-item">Inquiry Type: 
+            <?php 
+            $inquiry_types = [
+                'support' => 'Support',
+                'sales' => 'Sales',
+                'feedback' => 'Feedback'
+            ];
+            echo htmlspecialchars($inquiry_types[$inquiry_type] ?? 'Unknown');
+            ?>
+        </li>
+    </ul>
+</div>
 </body>
 </html>
